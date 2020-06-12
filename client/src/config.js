926a11daf3fc91d3941c68768b77e84b5fb8eedc
Bryce Morgan <bamorgan13@gmail.com>
2020-06-12 11:47:12 -0700

    Update imageUrl in client config

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,5 @@
-export const imageUrl = process.env.REACT_APP_IMAGE_URL || 'http://localhost:8000';
+export const imageUrl =
+	process.env.REACT_APP_IMAGE_URL || process.env.NODE_ENV === 'production'
+		? 'https://pokedex-redux-single-repo.herokuapp.com'
+		: 'http://localhost:8000';
 export const baseUrl = process.env.REACT_APP_BASEURL || `${imageUrl}/api`;
