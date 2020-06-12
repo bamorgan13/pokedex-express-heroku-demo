3e1e2564499b98c54a3880126564f7cd5474bd23
Bryce Morgan <bamorgan13@gmail.com>
2020-06-12 12:07:07 -0700

    Update postbuild script

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,8 @@
-export const imageUrl =
-	process.env.REACT_APP_IMAGE_URL || process.env.NODE_ENV === 'production'
-		? 'https://pokedex-redux-single-repo.herokuapp.com'
-		: 'http://localhost:8000';
+export const imageUrl = process.env.REACT_APP_IMAGE_URL || 'http://localhost:8000';
 export const baseUrl = process.env.REACT_APP_BASEURL || `${imageUrl}/api`;
+
+// export const imageUrl =
+// 	process.env.REACT_APP_IMAGE_URL || process.env.NODE_ENV === 'production'
+// 		? 'https://pokedex-redux-single-repo.herokuapp.com'
+// 		: 'http://localhost:8000';
+// export const baseUrl = process.env.REACT_APP_BASEURL || `${imageUrl}/api`;
