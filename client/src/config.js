09e79788807eeace6876bc95d9e6523429d936f9
Bryce Morgan <bamorgan13@gmail.com>
2020-06-12 12:19:49 -0700

    Revert config vars to NODE_ENV based values

--- client/src/config.js
+++ client/src/config.js
@@ -1,12 +1,5 @@
-import runtimeEnv from '@mars/heroku-js-runtime-env';
-
-const env = runtimeEnv();
-
-export const imageUrl = env.REACT_APP_IMAGE_URL || 'http://localhost:8000';
-export const baseUrl = env.REACT_APP_BASEURL || `${imageUrl}/api`;
-
-// export const imageUrl =
-// 	process.env.REACT_APP_IMAGE_URL || process.env.NODE_ENV === 'production'
-// 		? 'https://pokedex-redux-single-repo.herokuapp.com'
-// 		: 'http://localhost:8000';
-// export const baseUrl = process.env.REACT_APP_BASEURL || `${imageUrl}/api`;
+export const imageUrl =
+	process.env.REACT_APP_IMAGE_URL || process.env.NODE_ENV === 'production'
+		? 'https://pokedex-redux-single-repo.herokuapp.com'
+		: 'http://localhost:8000';
+export const baseUrl = process.env.REACT_APP_BASEURL || `${imageUrl}/api`;
